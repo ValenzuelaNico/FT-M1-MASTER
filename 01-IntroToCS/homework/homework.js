be40230bf0563eafa284348c0a2f1fb1e4041359
nicolas <valenzuela-nicolas@hotmail.com>
2022-11-09 09:30:15 -0300

    miercoles

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,21 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  let resultado = 0;
+  for (let i = 0; i < num.length ; i++){
+    resultado += +num[i] * 2 ** (num.length -1 - i )
+    
+  }
+  return resultado
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  var binario = "";
+    while (num > 0) {
+        binario = num % 2 + binario;
+        num = Math.floor(num - num / 2)
+    }
+    return binario;
 
 }
 
